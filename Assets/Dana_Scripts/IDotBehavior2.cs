3f410f445b6498a9f3661828508e7ad1cf107e91
rwanish <danatorrano@gmail.com>
2023-09-12 17:45:52 +0200

    fichier ajouté

---
+++ Assets/Dana_Scripts/IDotBehavior2.cs
@@ -0,0 +1,19 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;                                                                                                                                                                                                                                                                                                           
+
+public class IDotBehavior2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOMoveY(200, 1).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+} 
