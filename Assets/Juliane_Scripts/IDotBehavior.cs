808e5770f7f61521eb04b03398f98038ccfdee61
Juliane delcher <juliane.delcher@gmail.com>
2023-09-13 14:39:30 +0200

    Dot color change

--- Assets/Juliane_Scripts/IDotBehavior.cs
+++ Assets/Juliane_Scripts/IDotBehavior.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 
 public class IDotBehavior : MonoBehaviour
 {
+    public SpriteRenderer maVariable;
+    public Color color = Color.cyan;
     // Start is called before the first frame update
     void Start()
     {
-        transform.DOMoveY(200, 1).SetLoops(-1, LoopType.Yoyo);
+        transform.DOMoveY(200,2).SetLoops(-1, LoopType.Yoyo);
+        maVariable.DOColor(color, 2).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
     }
 
     // Update is called once per frame
