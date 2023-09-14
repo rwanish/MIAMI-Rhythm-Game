82f3562d48a55d63289c4519758ad4473b03f59c
rwanish <danatorrano@gmail.com>
2023-09-14 19:35:09 +0200

    Code change

--- Assets/Dana_Scripts/M1Rotation.cs
+++ Assets/Dana_Scripts/M1Rotation.cs
@@ -6,25 +6,38 @@ using DG.Tweening;
 public class M1Rotation : MonoBehaviour
 {
     float rotation = 90;
+
+    public float coolDown = 5f;
+    public float durationCoolDown = 5f;
+    
+
     // Start is called before the first frame update
     void Start()
     {
        
     }
 
+    private void Update()
+    {
+        coolDown -= Time.deltaTime;
+    }
+
     public void M1Wheel()
     {
+        if (coolDown > 0f) return;
         if (DOTween.IsTweening(transform)) return;
         transform.DORotate(transform.eulerAngles + new Vector3(0, 0, rotation), 1);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if (coolDown > 0f) return;
         if (collision.gameObject.tag == "Player")
         {
-            transform.rotation = Quaternion.Euler(Vector3.forward * 0);
-            
-            
+            transform.DOKill();
+            transform.DORotate(new Vector3(0, 0, 0), 1);
+
+            coolDown = durationCoolDown;
         }
     }
 }
